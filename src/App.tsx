b4a73bcf2714ecc0506a389a5fa38e08fe54c5f7
PraiseOrly <praiseorly.a@gmail.com>
2026-01-14 22:43:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './pages/homepage/Homepage';
-import AuthPage from './pages/shared/AuthPage';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import DoctorDashboard from './pages/doctor_app/DoctorDashboard';
-import PatientDashboard from './pages/patient_app/PatientDashboard';
+import Blog from './pages/homepage/Blog';
 import ContactPage from './pages/homepage/ContactPage';
 import DoctorBenefits from './pages/homepage/DoctorBenefits';
+import Homepage from './pages/homepage/Homepage';
 import PatientBenefits from './pages/homepage/PatientBenefits';
+import PatientDashboard from './pages/patient_app/PatientDashboard';
+import AuthPage from './pages/shared/AuthPage';
 import ErrorPage from './pages/shared/ErrorPage';
-import NotFoundPage from './pages/shared/NotFoundPage';
 
-import Research from './pages/homepage/Research';
-import ArrhythmiaDetection from './pages/homepage/ArrhythmiaDetection';
-import EcgAnalysis from './pages/shared/EcgAnalysis';
-import Certifications from './pages/homepage/Certifications';
-import RiskAssessment from './pages/shared/RiskAssessment';
-import LongitudinalTracking from './pages/shared/LongitudinalTracking';
-import EhrIntegration from './pages/homepage/EhrIntegration';
-import TreatmentGuidelines from './pages/homepage/TreatmentGuidelines';
-import LiveAssistant from './pages/shared/LiveAssistant';
-import LiveMessageAssistant from './components/shared/LiveMessageAssistant';
-import ErrorBoundary from './components/shared/ErrorBoundary';
-import Loading from './components/shared/Loading';
 import Footer from './components/Homepage/Footer';
+import ErrorBoundary from './components/shared/ErrorBoundary';
+import LiveMessageAssistant from './components/shared/LiveMessageAssistant';
 import { UserProvider } from './context/UserContext';
+import ArrhythmiaDetection from './pages/homepage/ArrhythmiaDetection';
+import Certifications from './pages/homepage/Certifications';
+import EhrIntegration from './pages/homepage/EhrIntegration';
+import Research from './pages/homepage/Research';
+import TreatmentGuidelines from './pages/homepage/TreatmentGuidelines';
+import EcgAnalysis from './pages/shared/EcgAnalysis';
+import LiveAssistant from './pages/shared/LiveAssistant';
+import LongitudinalTracking from './pages/shared/LongitudinalTracking';
+import RiskAssessment from './pages/shared/RiskAssessment';
 import './styles/homepage.css';
 
 export function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return <ErrorBoundary>
     <UserProvider>
       <Router>
@@ -50,6 +34,7 @@ export function App() {
           <Route path="/doctor-dashboard/*" element={<DoctorDashboard />} />
           <Route path="/patient-dashboard/*" element={<PatientDashboard />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/blog" element={<Blog />} />
           <Route path="/for-doctors" element={<DoctorBenefits />} />
           <Route path="/for-patients" element={<PatientBenefits />} />
           <Route path="/error" element={<ErrorPage />} />
